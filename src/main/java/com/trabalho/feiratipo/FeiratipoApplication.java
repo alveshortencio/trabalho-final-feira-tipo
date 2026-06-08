a15dfd0d02fe43f7a038b466dc4f2e95d544d6bc
Gabriel <galveshortencio@gmail.com>
2026-06-07 22:52:06 -0300

    Sprint 1: Ambiente e Backend Inicial

---
+++ src/main/java/com/trabalho/feiratipo/FeiratipoApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.feiratipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiratipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiratipoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalho/feiratipo/FeiratipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.feiratipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiratipoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
